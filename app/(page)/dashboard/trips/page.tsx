cf5843e9719e29f46abac989d07c0ef4f6453e46
RidmiAhinshika <ridmi2.desilva@gmail.com>
2025-10-05 11:33:12 +0530

    feat: Add Trending News sidebar to AllTripsTable component

--- app/(page)/dashboard/trips/page.tsx
+++ app/(page)/dashboard/trips/page.tsx
@@ -364,7 +364,11 @@ export default function AllTripsTable() {
       </div>
     </div>
 
-    
+    {/* Trending News Sidebar - Takes up 1/3 of the space on large screens */}
+    <div className="xl:col-span-1">
+      <TrendingNews maxItems={8} showCompact={false} />
+    </div>
+  </div>
 
       {/* Drawer, Modal & Prompt remain the same as your original code */}
       {/* Edit Drawer */}
